b84692d4a07bba8846524f0d3d92d84bf7931a2d
Nick Miller <nick.miller@onyxpoint.com>
2018-02-09 16:15:03 -0500

    reset to more verbose hash format

--- spec/defines/system_table_spec.rb
+++ spec/defines/system_table_spec.rb
@@ -11,8 +11,8 @@ describe 'incron::system_table' do
         context 'with path and command only' do
           let(:title) { 'path_and_command_test' }
           let(:params) {{
-            path: '/some/valid/path',
-            command: '/some/valid/command',
+            :path => '/some/valid/path',
+            :command => '/some/valid/command',
           }}
           let(:expected) { "/some/valid/path IN_MODIFY,IN_MOVE,IN_CREATE,IN_DELETE /some/valid/command\n" }
           it { is_expected.to create_file('/etc/incron.d/path_and_command_test').with_content(expected) }
@@ -21,7 +21,7 @@ describe 'incron::system_table' do
         context 'with custom_content only' do
           let(:title) { 'custom_content_test' }
           let(:params) {{
-            custom_content: "totally valid incron content\n"
+            :custom_content => "totally valid incron content\n"
           }}
           let(:expected) { "totally valid incron content\n" }
           it { is_expected.to create_file('/etc/incron.d/custom_content_test').with_content(expected) }
