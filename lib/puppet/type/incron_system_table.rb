08ddc9f5922f90a2a590bd33fbebf26a1ea5dd99
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-05-04 17:42:41 -0400

    (SIMP-4828) Add incron_system_table native type
    - Added a native type `incron_system_table` to allow for client side
      glob expansion on paths
    - Pushed all validation of parameters off to the native type from
      `incron::system_table`
    - Updated acceptance tests to verify that incron is actually working
    - Bumped the supported puppet version to '< 6'
    
    SIMP-4828 #comment supporting incron work needed for 'puppet generate types'

---
+++ lib/puppet/type/incron_system_table.rb
@@ -0,0 +1,168 @@
+Puppet::Type.newtype(:incron_system_table) do
+  @doc = <<-EOM
+  Creates an 'incrond' compatible system table
+
+  Line order will be preserved
+
+  Any paths that contain globs '*' will be expanded into the appropriate number
+  of rules based on glob expansion on the target system
+
+  Globbed paths that do not result in a valid path expansion will be ignored
+
+  Globs starting with /** will also be ignored due to the burden placed on the system
+
+  WARNING: TAKE CARE THAT YOU KNOW WHAT YOUR GLOBS WILL EXPAND TO!
+  EOM
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:name) do
+    isnamevar
+    desc 'The filename to use for the table - Non-word characters will be replaced'
+
+    munge do |value|
+
+      # This is here for the use of later parameters and properties
+      @valid_incron_masks = [
+        'IN_ACCESS',
+        'IN_ALL_EVENTS',
+        'IN_ATTRIB',
+        'IN_CLOSE',
+        'IN_CLOSE_NOWRITE',
+        'IN_CLOSE_WRITE',
+        'IN_CREATE',
+        'IN_DELETE',
+        'IN_DELETE_SELF',
+        'IN_DONT_FOLLOW',
+        'IN_MODIFY',
+        'IN_MOVE',
+        'IN_MOVED_FROM',
+        'IN_MOVE_SELF',
+        'IN_NO_LOOP',
+        'IN_ONESHOT',
+        'IN_ONLYDIR',
+        'IN_OPEN'
+      ]
+      self.class.send(:attr_reader, 'valid_incron_masks')
+
+      value.split(/\W+/).join('__')
+    end
+  end
+
+  newparam(:path, :array_matching => :all, :parent => Puppet::Parameter::Path) do
+    desc 'Path(s) to watch and apply the `command`'
+
+    accept_arrays(true)
+
+    munge do |value|
+      Array(value)
+    end
+  end
+
+  newparam(:mask, :array_matching => :all) do
+    desc 'The incron "masks" to apply'
+
+    munge do |value|
+      Array(value).join(',')
+    end
+
+    validate do |value|
+      value = value.split(',') if value.is_a?(String)
+
+      invalid_masks = (value - resource.parameters[:name].valid_incron_masks)
+
+      unless invalid_masks.empty?
+        raise(Puppet::Error, %(Invalid masks '#{invalid_masks.join("', '")}'. Masks must be one of '#{resource.parameters[:name].valid_incron_masks.join("', '")}'.))
+      end
+    end
+  end
+
+  newparam(:command, :array_matching => :all, :parent => Puppet::Parameter::Path) do
+    desc <<-EOM
+    The command(s) to apply when the paths change
+
+    If multiple paths and commands are specified, they will create multiple
+    lines that contain all possible combinations
+    EOM
+
+    accept_arrays(true)
+
+    munge do |value|
+      Array(value)
+    end
+  end
+
+  newparam(:content, :array_matching => :all) do
+    desc 'Raw content to add to the file - Will be validated'
+
+    errors = []
+
+    validate do |values|
+      Array(values).each do |value|
+        path, mask, command = value.split(/\s+/)
+
+        unless (path && path.start_with?('/'))
+          errors << "Path '#{path}' is not an absolute path"
+        end
+
+        unless (mask && (mask.split(',') -  resource.parameters[:name].valid_incron_masks).empty?)
+          errors << %(Masks must be some combination of '#{resource.parameters[:name].valid_incron_masks.join("', '")}')
+        end
+
+        unless (command && command.start_with?('/'))
+          errors << "Command '#{command}' is not an absolute path"
+        end
+      end
+    end
+
+    munge do |value|
+      Array(value).map(&:lines).flatten.map(&:strip)
+    end
+  end
+
+  validate do
+    unless self[:ensure] == :absent
+      unless self[:content] || (self[:path] && self[:mask] && self[:command])
+        raise(Puppet::Error,'You must specify either "content" or "path", "mask", and "command"')
+      end
+
+      if self[:content] && (self[:path] || self[:mask] || self[:command])
+        raise(Puppet::Error,'You cannot specify "content" with "path", "mask", or "command"')
+      end
+    end
+  end
+
+  autorequire(:package) do
+    ['incron']
+  end
+
+  autorequire(:file) do
+    to_req = []
+
+    # Require all commands that we're going to run
+    if self[:command]
+      self[:command].each do |cmd|
+        unless cmd.include?('*')
+          to_req << cmd
+        end
+      end
+    end
+
+    # Grab the commands out of 'content' lines and required those as well
+    if self[:content]
+      self[:content].join.each_line do |content|
+
+        cmd = content.split(/\s+/)[2]
+
+        unless cmd.include?('*')
+          to_req << cmd
+        end
+      end
+    end
+
+    to_req
+  end
+end
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,15 +13,16 @@ class incron (
   Hash          $system_table   = {},
   String        $package_ensure = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' }),
 ) {
+  package { 'incron': ensure => $package_ensure }
 
   $system_table.each |String $name, Hash $values| {
-    ::incron::system_table { $name: * => $values }
+    incron::system_table { $name: * => $values }
   }
 
   $users.each |String $user| {
-    ::incron::user { $user: }
+    incron::user { $user: }
   }
-  ::incron::user { 'root': }
+  incron::user { 'root': }
 
   concat { '/etc/incron.allow':
     owner          => 'root',
@@ -31,12 +32,13 @@ class incron (
     warn           => true
   }
 
-  file { '/etc/incron.deny':
-    ensure => 'absent'
-  }
+  file { '/etc/incron.deny': ensure => 'absent' }
 
-  package { 'incron':
-    ensure => $package_ensure
+  file { '/etc/incron.d':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0640'
   }
 
   # service installed with wrong permissions from rpm.
